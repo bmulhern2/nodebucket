95b64be662ad61fea7f1fed0fad79f4b0431d71f
System Administrator <bmulhern09@gmail.com>
2020-10-01 11:22:42 -0400

    implemented api routes

--- server/routes/employeeRoutes.js
+++ server/routes/employeeRoutes.js
@@ -22,4 +22,154 @@ try {
 }
 })
 
+// Employee Read Task Route
+
+router.get('/:empId/tasks', function(req, res) {
+try {
+    employee.findOne({ "empId": req.params.empId }, function(err, employee) {
+        if (err) {
+            console.log(err)
+            res.status(500).send({
+                'message': 'internal server error'
+            })
+        } else {
+            console.log(employee)
+            res.json(employee)
+        }
+    })
+} catch (e) {
+    console.log(e)
+    res.status(500).send({
+        'message': 'internal server error'
+    })
+}
+})
+// New Task Route
+router.post('/:empID/tasks/', async(req, res) => {
+    try {
+        employee.findOne({ "empId": req.params.empId }, function(err, employee) {
+            if (err) {
+                console.log(err)
+                res.status(500).send({
+                    "message": "internal server error"
+                })
+            } else {
+                console.log(employee)
+
+                const task = {
+                    text: req.body.text
+                }
+
+                employee.todo.push(task)
+
+                employee.save(function(err, updatedEmployee) {
+                    if (err) {
+                        console.log(err)
+                        res.status(500).send({
+                            "message": "internal server error"
+                        })
+                    } else {
+                        console.log(updatedEmployee)
+
+                        res.json(updatedEmployee)
+                    }
+                })
+            }
+        })
+    } catch (e) {
+        console.log(e)
+
+        res.status(500).send({
+            "message": "internal server error"
+        })
+    }
+})
+// Update Task Route
+router.put('/:empId/tasks', async(req, res) => {
+   try {
+    employee.findOne({ "empId": res.params.empId }, function(err, employee) {
+        if (err) {
+            console.log(err)
+            res.status(500).send({
+                "message": "internal server error"
+            })
+        } else {
+            console.log(employee)
+            employee.set({
+                todo: req.body.todo,
+                done: req.body.done
+            })
+            employee.save(function(err, updatedEmployee) {
+                if (err) {
+                    console.log(err)
+                    res.status(500).send({
+                        "message": "internal server error"
+                    })
+                } else {
+                    console.log(updatedEmployee)
+                    res.json(updatedEmployee)
+                }
+            })
+        }
+    })
+} catch (e) {
+    console.log(e)
+    res.status(500).send({
+        'message': 'internal server error'
+    })
+    }
+})
+// Delete Task Route
+router.delete('/:empId/tasks/:tasksId', async(req, res) => {
+    try {
+        employee.findOne({ 'empId': req.params.empId }, function(err, employee) {
+            if (err) {
+                console.log(err)
+                res.status(500).send({
+                    'message': 'internal server error'
+                })
+            } else {
+                console.log(employee)
+                const todoItem = employee.todo.find(item => item._id.tostring() === req.params.taskId)
+                const doneItem = employee.done.find(item => item._id.toString() === req.params.taskId) 
+                if (todoItem) {
+                    employee.todo.id(todoItem._id).remove()
+                    employee.save(function(err, updatedtodoItemEmployee) {
+                        if (err) {
+                            console.log(err)
+                            res.status(500).send({
+                                'message': 'internal server error'
+                            })
+                        } else {
+                            console.log(updatedtodoItemEmployee)
+                            res.json(updatedtodoItemEmployee)
+                        }
+                    })
+                } else if (doneItem) {
+                    employee.done.id(doneItem._id).remove()
+                    employee.save(function(err, updatedDoneItemEmployee) {
+                        if (err) {
+                            console.log(err)
+                            res.status(500).send({
+                                'message': 'internal server error'
+                            })
+                        } else {
+                            console.log(updatedDoneItemEmployee)
+                            res.send("Delete successful")
+                        }
+                    })
+                } else {
+                    console.log("Invalid Task Id")
+                    res.status(200).send("Unable to located the requested task")
+                }
+            }
+        })
+    } catch (e) {
+        console.log(e)
+        res.status(500).send({
+            'message': 'internal server error'
+        })
+    }
+})
+
 module.exports = router;
