99d37fdd40ebd3d2dada7876563d8deebe796620
System Administrator <bmulhern09@gmail.com>
2020-10-07 11:54:33 -0400

    added 404 reference

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,14 +1,15 @@
-import {Routes} from '@angular/router';
-import {BaseLayoutComponent} from './shared/base-layout/base-layout.component';
-import {HomeComponent} from './pages/home/home.component';
 /*
 Title: nodebucket
 Author: Professor Krasso
 Date: 9/24/2020
 Modified By: Brendan Mulhern
+Reference: https://stackoverflow.com/questions/36260839/angular-2-how-to-redirect-to-404-or-other-path-if-the-path-does-not-exist
 Description: This is nodebucket; a task manager application.
 */
 
+import {Routes} from '@angular/router';
+import {BaseLayoutComponent} from './shared/base-layout/base-layout.component';
+import {HomeComponent} from './pages/home/home.component';
 import { LoggedInComponent } from './logged-in/logged-in.component'
 import { AuthGuardGuard } from './auth-guard.guard';
 import { NotFoundComponent } from './not-found/not-found.component'
